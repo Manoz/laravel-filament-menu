f9f3bda74cb26d7244e0d04f93d613a14c038a2f
Kévin Legrand <legrand@novius.fr>
2025-10-17 08:35:51 +0200

    fix class/attributes

--- resources/views/menu-item.blade.php
+++ resources/views/menu-item.blade.php
@@ -6,34 +6,28 @@
     /** @var Menu $menu */
     /** @var MenuItem $item */
 @endphp
-<li @class([
-    'lfm-item-li',
-    $item->children->isNotEmpty() ? 'lfm--has-children' : '',
-])>
+<li class="lfm-item-li" @if ($item->children->isNotEmpty()) data-has-children="true" @endif>
     @if ($item->link_type === LinkType::html)
         {!! $item->html !!}
     @elseif ($item->link_type !== LinkType::empty)
         <a href="{{ $item->href() }}"
-            @class([
-                'lfm-item',
-                'lfm-item--active' => $menu->template->isActiveItem($item),
-                $item->html_classes
-            ])
-            {{ $item->target_blank ? 'target="_blank"' : '' }}
+            @class([ 'lfm-item', $item->html_classes])
+            @if ($item->target_blank) target="_blank" rel="noopener noreferrer" @endif
+            @if ($menu->template->isActiveItem($item)) data-active="true" @endif
         >
             {{ $item->title }}
         </a>
     @else
-        <{{$itemEmptyTag}} @class(['lfm-item lfm-item--is-empty', $item->html_classes])>
+        <{{$itemEmptyTag}} @class(['lfm-item', $item->html_classes])>
             {{ $item->title }}
         </{{$itemEmptyTag}}>
     @endif
 
     @if ($item->children->isNotEmpty())
-        <ul @class([
-            'lfm-items-container',
-            $menu->template->containsActiveItem($item) ? 'lfm--has-active-item' : '',
-        ]) data-depth="{{ $item->depth + 1 }}">
+        <ul class="lfm-items-container"
+            data-depth="{{ $item->depth + 1 }}"
+            @if ($menu->template->containsActiveItem($item)) data-active-items="true" @endif
+        >
             @foreach($item->children as $item)
                 {!! $menu->template->renderItem($menu, $item, $itemEmptyTag) !!}
             @endforeach
